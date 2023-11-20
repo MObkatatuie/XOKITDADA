2a7f0d8636651cf0c328fafb1747724d36298536
MObkatatuie <151482769+MObkatatuie@users.noreply.github.com>
2023-11-20 12:20:46 +0700

    Update your-script.js

--- your-script.js
+++ your-script.js
@@ -1,53 +1,114 @@
-const makeAIMove = () => {
-    // ตรวจสอบการเดาที่ชนะหรือการบล็อก
-    const winningMove = findWinningMove('O');
-    const blockingMove = findWinningMove('X');
+// Game variables
+let currentPlayer = 'X';
+let isGameOver = false;
+let board = ['', '', '', '', '', '', '', '', ''];
 
-    if (winningMove !== -1) {
-        makeMove(winningMove);
-    } else if (blockingMove !== -1) {
-        makeMove(blockingMove);
-    } else {
-        // พิจารณาตำแหน่งกลางก่อน, จากนั้นถึงมุม, แล้วถึงขอบ
-        const center = 4;
-        if (board[center] === '') {
-            makeMove(center);
+// Function to check for a win
+const checkWin = () => {
+    const winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]             // Diagonals
+    ];
+
+    for (let pattern of winPatterns) {
+        const [a, b, c] = pattern;
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            return true; // We have a winner
+        }
+    }
+
+    return false; // No winner yet
+};
+
+// Function to check for a tie
+const checkTie = () => {
+    return board.every(cell => cell !== '');
+};
+
+// Function to handle cell click
+const handleCellClick = (event) => {
+    const cellIndex = event.target.dataset.index;
+
+    // Check if the cell is empty and the game is not over
+    if (board[cellIndex] === '' && !isGameOver) {
+        board[cellIndex] = currentPlayer;
+        renderBoard();
+
+        // Check for a win
+        if (checkWin()) {
+            document.getElementById('message').textContent = `${currentPlayer} wins!`;
+            isGameOver = true;
+        } else if (checkTie()) {
+            document.getElementById('message').textContent = 'It\'s a tie!';
+            isGameOver = true;
         } else {
-            const corners = [0, 2, 6, 8].filter(i => board[i] === '');
-            const edges = [1, 3, 5, 7].filter(i => board[i] === '');
-            const availableMoves = corners.length > 0 ? corners : edges;
-            
-            if (availableMoves.length > 0) {
-                const randomIndex = Math.floor(Math.random() * availableMoves.length);
-                const aiMove = availableMoves[randomIndex];
-                makeMove(aiMove);
+            // Switch player
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+
+            // If the next move is AI's, make the move
+            if (currentPlayer === 'O' && !isGameOver) {
+                makeAIMove();
             }
         }
     }
 };
 
-const findWinningMove = (player) => {
-    // ตรวจสอบที่เดาที่ชนะหรือการบล็อก
-    for (let i = 0; i < 9; i++) {
-        if (board[i] === '') {
-            board[i] = player;
-            if (checkWin()) {
-                board[i] = ''; // รีเซ็ตที่เดา
-                return i;
-            }
-            board[i] = ''; // รีเซ็ตที่เดา
-        }
-    }
-    return -1;
+// Function to render the game board
+const renderBoard = () => {
+    const boardElement = document.querySelector('.board');
+    boardElement.innerHTML = '';
+
+    board.forEach((cell, index) => {
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('cell');
+        cellElement.textContent = cell;
+        cellElement.dataset.index = index;
+        boardElement.appendChild(cellElement);
+    });
 };
 
-const makeMove = (index) => {
-    board[index] = 'O';
+// Function for AI to make a move
+const makeAIMove = () => {
+    // Get empty cells
+    const emptyCells = board.reduce((acc, cell, index) => {
+        if (cell === '') {
+            acc.push(index);
+        }
+        return acc;
+    }, []);
+
+    // Randomly choose an empty cell
+    const randomIndex = Math.floor(Math.random() * emptyCells.length);
+    const aiMove = emptyCells[randomIndex];
+
+    // Make the AI move after a short delay to make it more human-like
+    setTimeout(() => {
+        board[aiMove] = currentPlayer;
+        renderBoard();
+
+        // Check for a win after AI's move
+        if (checkWin()) {
+            document.getElementById('message').textContent = 'AI wins!';
+            isGameOver = true;
+        } else if (checkTie()) {
+            document.getElementById('message').textContent = 'It\'s a tie!';
+            isGameOver = true;
+        } else {
+            // Switch player
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
+    }, 500);
+};
+
+// Function to reset the game
+const resetGame = () => {
+    currentPlayer = 'X';
+    isGameOver = false;
+    board = ['', '', '', '', '', '', '', '', ''];
+    document.getElementById('message').textContent = '';
     renderBoard();
-    checkGameStatus();
 };
-
-// ...
 
 // Function to initialize the game
 const initGame = () => {
@@ -63,6 +124,3 @@ const initGame = () => {
 
 // Initialize the game
 initGame();
-
-// ...
-
